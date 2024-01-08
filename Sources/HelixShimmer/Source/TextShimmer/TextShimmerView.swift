e1b0a3007e401bae03c7f919d364386a9934b6d4
armansarvardin <arman.sarvardin@zimran.io>
2024-01-08 15:30:12 +0600

    refactor: removed unnecessary code

--- Sources/HelixShimmer/Source/TextShimmer/TextShimmerView.swift
+++ Sources/HelixShimmer/Source/TextShimmer/TextShimmerView.swift
@@ -8,49 +8,6 @@
 import Foundation
 import SwiftUI
 
-struct TextShimmerView : View {
-    @State private var show = false
-    let center = UIScreen.main.bounds.width
-    
-    var body : some View {
-        ZStack {
-            Color.white
-            Color.black.opacity(0.08)
-            Color.white
-                .mask(
-                    Capsule()
-                        .fill(
-                            LinearGradient(
-                                gradient: .init(
-                                    colors: [
-                                        .clear,
-                                        Color.white.opacity(0.75),
-                                        .clear
-                                    ]
-                                ),
-                                startPoint: .top,
-                                endPoint: .bottom
-                            )
-                        )
-                        .rotationEffect(.init(degrees: 65))
-                        .offset(x: self.show ? center : -center)
-                        .padding(-100)
-                )
-        }
-        .onAppear {
-            withAnimation(
-                Animation.easeInOut
-                    .speed(0.15)
-                    .delay(0)
-                    .repeatForever(autoreverses: false)
-            ) {
-                self.show.toggle()
-            }
-        }
-    }
-}
-
-
 public struct ShimmedTextView: View {
     let isLoading: Bool
     let shimmerLength: Int
@@ -58,7 +15,10 @@ public struct ShimmedTextView: View {
     public var body: some View {
         if isLoading {
             Text(getShimmerText())
-                .shimmer(isLoading: isLoading, shape: .capsule)
+                .shimmer(
+                    isLoading: isLoading,
+                    shape: .capsule
+                )
         }
     }
     
